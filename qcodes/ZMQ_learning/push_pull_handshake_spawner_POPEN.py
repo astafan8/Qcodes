082bb953cc503388ec065e01ffc190f4a2746e83
WilliamHPNielsen <whpn@mailbox.org>
2018-11-02 11:29:39 +0100

    Add sort of maybe working example code

---
+++ qcodes/ZMQ_learning/push_pull_handshake_spawner_POPEN.py
@@ -0,0 +1,122 @@
+import subprocess
+from time import sleep, perf_counter
+from typing import Tuple
+
+import sys
+import zmq
+from zmq.sugar.socket import Socket  # for type annotations
+
+
+# This is the main (mother) process, also the pusher
+
+def is_the_writer_online(poller: zmq.Poller,
+                         req_sock: Socket,
+                         timeout: float) -> bool:
+    """
+    Ask the writer for a reply
+
+    Args:
+        poller: a Poller with the req socket
+        req_sock: the req socket. Should be in poller
+        timeout: poll timeout in SECONDS
+    """
+
+    print('Asking writer "are you alive?"')
+
+    # now check whether we should spawn the writer
+    req_sock.send(b'Are you there?')
+
+    response = dict(poller.poll(timeout=1000*timeout))  # timeout in ms
+
+    if req_sock in response:
+        print(f'Got poll response: {response}')
+        print(req_sock.recv())
+        return True
+    else:
+        print('No writer found')
+        return False
+
+
+def reset_req_sock_and_poller(poller: zmq.Poller,
+                              req_sock: Socket,
+                              req_port: int) -> Tuple[zmq.Poller, Socket]:
+    """
+    Reset socket and poller for reuse
+    """
+    req_sock.setsockopt(zmq.LINGER, 0)
+    req_sock.close()
+    poller.unregister(req_sock)
+
+    req_sock = ctx.socket(zmq.REQ)
+    req_sock.connect(f"tcp://127.0.0.1:{req_port}")
+    poller.register(req_sock, zmq.POLLIN)
+
+    return (poller, req_sock)
+
+
+def spawn_writer(pull_port: int, rep_port: int) -> None:
+    """
+    Spawn the writer. I dunno, should this return something?
+    """
+    print('Spawning out a writer')
+    cmd = ["python", "push_pull_handshake_suicidal_writer.py",
+           f"{pull_port}", f"{rep_port}"]
+    subprocess.Popen(cmd)
+
+
+port = 5557
+ctx = zmq.Context()
+
+if __name__ == "__main__":
+
+    # make the PUSH socket we'll use for pushing out data
+    push_sock = ctx.socket(zmq.PUSH)
+    push_sock.set_hwm(1)
+
+    # make the REQ socket we'll use for handshaking with the writer
+    req_sock = ctx.socket(zmq.REQ)
+
+    got_port = False
+    for n in range(1, 12, 2):
+        print(f'Trying to bind pusher and req on ports {port}, {port+1}')
+        try:
+            push_sock.bind(f"tcp://127.0.0.1:{port}")
+            req_sock.connect(f"tcp://127.0.0.1:{port+1}")
+            got_port = True
+            req_port = port+1
+            break
+        except:
+            port += 1
+
+    if not got_port:
+        print('No port available')
+        sys.exit()
+
+    poller = zmq.Poller()
+    poller.register(req_sock, zmq.POLLIN)
+
+    # now check if the writer is alive
+    # if it is, just proceed. If not, spawn it
+    if not is_the_writer_online(poller, req_sock, 0.1):
+
+        spawn_writer(port, req_port)
+
+        # then reset the req socket, since it is now "broken" from not having
+        # got a response
+        poller, req_sock = reset_req_sock_and_poller(poller, req_sock,
+                                                     req_port)
+
+        sleep(0.250)
+        if not is_the_writer_online(poller, req_sock, 1):
+            print('Failing to spawn writer. Sorry, bro.')
+            sys.exit()
+
+    # if we ever make it this far, there is a writer
+
+    for n in range(5):
+        message = f'This is message {n+1}'
+        push_sock.send(message.encode('utf-8'))
+        sleep(0.5)
+        print(f'Just sent message number {n+1}')
+
+    print("Spawner is dead")
--- qcodes/ZMQ_learning/push_pull_handshake_suicidal_writer.py
+++ qcodes/ZMQ_learning/push_pull_handshake_suicidal_writer.py
@@ -1,1 +1,69 @@
-#
+import zmq
+from time import perf_counter
+import os
+from datetime import datetime
+import argparse
+
+
+def suicidal_writer(pull_port: int, rep_port: int) -> None:
+    """
+    Listen for stuff and die after some time
+    """
+
+    ctx = zmq.Context()
+
+    # make the PULL socket we'll use for receiving the data
+    pull_sock = ctx.socket(zmq.PULL)
+    pull_sock.connect(f"tcp://127.0.0.1:{pull_port}")
+
+    # make the REP socket that we'll use for handshaking
+    rep_sock = ctx.socket(zmq.REP)
+    rep_sock.bind(f"tcp://127.0.0.1:{rep_port}")
+
+    poller = zmq.Poller()
+    poller.register(rep_sock)
+    poller.register(pull_sock)
+
+    last_ping = perf_counter()
+
+    timeout = 15  # timeout in seconds
+
+    timestr = datetime.now().strftime('%Y-%m-%d_%H_%M_%S')
+    filename = os.path.join(os.getcwd(), f'writer-{timestr}.txt')
+
+    # In the real application, ANOTHER file will be opened based on message
+    # content. This file is just for status "prints"
+    with open(filename, 'w') as fid:
+
+        fid.write(f'Got pull_port {pull_port} and req_port {rep_port}\n')
+
+        while not (perf_counter() - last_ping) > timeout:
+
+            response = dict(poller.poll(timeout=100))
+
+            if rep_sock in response:
+                mssg = rep_sock.recv().decode('utf-8')
+                fid.write(mssg + '\n')
+                rep_sock.send(b"Yes")
+                last_ping = perf_counter()
+            if pull_sock in response:
+                mssg = pull_sock.recv()
+                fid.write(mssg.decode('utf-8') + '\n')
+                last_ping = perf_counter()
+
+        fid.write('Reached my timeout limit. Signing off.')
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Be a suicidal writer')
+    parser.add_argument('pull_port', metavar='pull_port', type=int,
+                        help='port to pull for data')
+    parser.add_argument('rep_port', metavar='rep_port', type=int,
+                        help='port to reply to with status')
+    args = parser.parse_args()
+    pull_port = args.pull_port
+    rep_port = args.rep_port
+
+    suicidal_writer(pull_port, rep_port)
+
